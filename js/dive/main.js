4015a55d710c2d836f434592adc9826ef88e144c
LachlanStephan <ljstephan116@gmail.com>
2022-07-09 13:18:38 +1000

    part one day 2 - js done

---
+++ js/dive/main.js
@@ -0,0 +1,67 @@
+const fs = require("fs");
+
+const splitData = (data) => {
+  data = data.toString();
+  data = data.split("\n");
+  return data;
+}
+
+const formatData = (data) => {
+  return data.map((d) => {
+    details = d.split(" ");
+    let dir = details[0];
+    let am = details[1];
+
+    return {
+      "direction": dir,
+      "amount": am
+    }
+  });
+}
+
+const increaseValue = (curr, increase) => {
+  return curr + increase;
+}
+
+const decreaseValue = (curr, decrease) => {
+  return curr - decrease;
+}
+
+const getSubPosition = (data) => {
+  let horizontal = 0;
+  let depth = 0;
+
+  data.forEach((movement) => {
+    const dir = movement.direction.toString();
+    const am = Number(movement.amount);
+
+    switch(dir) {
+      case "forward": 
+        horizontal = increaseValue(horizontal, am);
+      break;
+
+      case "down": 
+        depth = increaseValue(depth, am);
+      break;
+
+      case "up": 
+        depth = decreaseValue(depth, am);
+      break;
+    }
+  });
+
+  return horizontal * depth;
+}
+
+const main = () => {
+  fs.readFile("./input.txt", "utf-8", (err, d) => {
+    if (err) throw err;
+
+    const split = splitData(d);
+    const data = formatData(split);
+    const result = getSubPosition(data);
+    console.log(result);
+  });
+}
+
+main();
